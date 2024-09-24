8e985e0b0aa7313a325dcaf96e7cc0a619ec5273
iambean-git <peb1021@naver.com>
2024-09-24 15:51:23 +0900

    useState

--- src/04/MyList.js
+++ src/04/MyList.js
@@ -2,16 +2,18 @@ import MyListData from './MyListData.json';
 import MyListItem from './MyListItem';
 
 export default function MyList() {
-    console.log(MyListData);
+    //console.log(MyListData);
     
-    MyListData.map( item => console.log(item.title ));
+    // MyListData.map( item => console.log(item.title ));
+    const tags = MyListData.map( item => <MyListItem 
+                                            key = {item.title}
+                                            title={item.title} 
+                                            content={item.content} 
+                                            img={item.imgUrl} 
+                                            />);
     return (
-        <div className='w-10/12 grid grid-cols-2 gap-4'>
-            <MyListItem/>
-            <MyListItem/>
-            <MyListItem/>
-            <MyListItem/>
-            
+        <div className='w-10/12 grid grid-cols-1 lg:grid-cols-2 gap-4'>
+            {tags}
         </div>
   )
 }
--- src/04/MyListItem.js
+++ src/04/MyListItem.js
@@ -1,24 +1,35 @@
-export default function MyListItem() {
+import { useState } from "react";
+
+export default function MyListItem({title, content, img}) {
+    const [n, setN] = useState(0);
+    
+    const heartClick = () => {
+        setN(n+1);
+        console.log('n is', n);
+    }
+
     return (
         <div className='w-full 
                             border border-gray-300
                             flex justify-center items-center'>
-            <div className='w-1/3 flex justify-center items-start'>
-                이미지
+            <div className='w-1/3 flex justify-start mb-auto items-start'>
+                <img src={img} alt={title} className='w-60 p-2' />
             </div>
-            <div className='w-2/3 flex flex-col justify-between items-center'>
+            <div className='w-2/3 py-2 pr-2 flex flex-col justify-between items-center '>
                 <div>
-                    <div>
-                        title
+                    <div className="h-7 text-xl font-bold text-blue-800">
+                        {title}
                     </div>
-                    <div>
-                        content
+                    <div className="h-32 text-sm">
+                        {content}
                     </div>
                 </div>
-                <div>
-                    <span>💗 </span>
-                    <span>좋아요 </span>
-                    <span>0</span>
+                <div className="flex justify-end ml-auto mr-1 ">
+                    <span className="cursor-pointer" onClick={heartClick}>
+                        💗 
+                    </span>
+                    <span className="mx-1 font-bold">좋아요 </span>
+                    <span>{n}</span>
                 </div>
             </div>
         </div>
