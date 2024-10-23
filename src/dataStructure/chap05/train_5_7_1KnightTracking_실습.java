406f8bef11e86bf615ddf9d58bba3966707022cf
iambean-git <peb1021@naver.com>
2024-10-23 14:16:15 +0900

    knife tour

--- src/dataStructure/chap05/train_5_7_1KnightTracking_실습.java
+++ src/dataStructure/chap05/train_5_7_1KnightTracking_실습.java
@@ -20,17 +20,17 @@ import java.util.Stack;
 enum knightMoves {NW, NE, EN, ES, SE, SW, WS, WN}
 
 class Offsets4 {
-	int a;
-	int b;
-	public Offsets4(int a, int b) {
-		this.a = a; this.b = b;
-	}
+   int a;
+   int b;
+   public Offsets4(int a, int b) {
+      this.a = a; this.b = b;
+   }
 }
 public class train_5_7_1KnightTracking_실습 {
-	static Offsets4[] moves = new Offsets4[8];//static을 선언하는 이유를 알아야 한다
-    static final int N = 4;
+   static Offsets4[] moves = new Offsets4[8];//static을 선언하는 이유를 알아야 한다
+    static final int N = 5;
 
-	
+   
     // 체스판 배열
     private static int[][] board = new int[N][N];
 
@@ -47,10 +47,10 @@ public class train_5_7_1KnightTracking_실습 {
 
     // 체스판을 초기화 (-1로 설정)
     private static void initializeBoard() {
-    	for(int i=0; i<board.length; i++) {
-    		for(int j=0; j<board[0].length; j++)
-    			board[i][j] = -1;
-    	}
+       for(int i=0; i<board.length; i++) {
+          for(int j=0; j<board[0].length; j++)
+             board[i][j] = -1;
+       }
     }
 
     // 체스판의 범위 내에서 유효한 움직임인지 확인
@@ -60,66 +60,84 @@ public class train_5_7_1KnightTracking_실습 {
 
     // 나이트 투어 알고리즘 (비재귀적으로 스택 사용)
     private static boolean solveKnightTracking(int startX, int startY) {
-    	for (int ia = 0; ia < 8; ia++)
-    		moves[ia] = new Offsets4(0, 0);//배열에 Offsets4 객체를 치환해야 한다.
-    	moves[0].a = -2;	moves[0].b = -1;	//NW으로 이동
-    	moves[1].a = -2;	moves[1].b = 1;		//NE
-    	moves[2].a = -1;	moves[2].b = 2;		//EN
-    	moves[3].a = 1;		moves[3].b = 2;		//ES
-    	moves[4].a = 2;		moves[4].b = 1;		//SE
-    	moves[5].a = 2;		moves[5].b = -1;	//SW
-    	moves[6].a = -1;	moves[6].b = -2;	//WS
-    	moves[7].a = 1;		moves[7].b = -2;	//WN
+       for (int ia = 0; ia < 8; ia++)
+          moves[ia] = new Offsets4(0, 0);//배열에 Offsets4 객체를 치환해야 한다.
+       moves[0].a = -2;   moves[0].b = -1;   //NW으로 이동
+       moves[1].a = -2;   moves[1].b = 1;      //NE
+       moves[2].a = -1;   moves[2].b = 2;      //EN
+       moves[3].a = 1;      moves[3].b = 2;      //ES
+       moves[4].a = 2;      moves[4].b = 1;      //SE
+       moves[5].a = 2;      moves[5].b = -1;   //SW
+       moves[6].a = 1;      moves[6].b = -2;   //WS
+       moves[7].a = -1;   moves[7].b = -2;   //WN
         // 나이트가 이동할 수 있는 8가지 방향
         
         Stack<Point> stack = new Stack<>();
-        int count = 0;
+        int count = 1;
 
         // 시작 위치를 스택에 푸시
         stack.push(new Point(startX, startY, 0));
         board[startX][startY] = 0; // 시작 위치는 첫 번째 이동
-        int moveNum = stack.peek().moveToward;
         
+       
         while (!stack.isEmpty()) {
-            if(count==16) {
-            	System.out.println("끝");
-            	break;
-            }
-            
-            int moveX = moves[moveNum].a;
-        	int moveY = moves[moveNum].b;
-            
-            if(isSafe(startX+moveX, startY+moveY)) {	//다음칸 갈수없음
-            	
-            } else {	//다음칸 갈수있음
-            	startX += moveX;
-        		startY += moveY;
-        		board[startX][startY] = 0;
-        		stack.push(new Point(startX, startY,0));            	
-            }
-            
+           //showData();
+           Point current = stack.peek();
+           int curX = current.x;
+           int curY = current.y;
+           boolean moveCheck = false;
            
            
+           for(int i=current.moveToward; i<8; i++) {
+              int nextX = curX + moves[i].a;
+              int nextY = curY + moves[i].b;
+              
+              if(isSafe(nextX, nextY)) {
+                 stack.push(new Point(nextX, nextY, 0));
+                 board[nextX][nextY] = count++;
+                 current.moveToward = i + 1;
+                 moveCheck = true;
+                 break;
+              }
+           } 
+           
+           //현재 위치에서 이동할 수 있는 곳이 없음
+           if (!moveCheck) {
+              Point removed = stack.pop();
+              board[removed.x][removed.y] = -1;
+              count--;
+           }
+           
+           if(count==N*N) {
+              return true;
+           }
         }
-
         return false; // 해결하지 못함
     }
 
+
     // 결과 출력
     private static void showTracking() {
+       for(int i=0; i<board.length; i++) {
+          for(int j=0; j<board[0].length; j++) {
+             System.out.print(board[i][j] + " ");
+          }
+          System.out.println();
+       }
+       System.out.println();
 
     }
     
     
     //배열출력
     private static void showData() {
-    	for(int i=0; i<board.length; i++) {
-    		for(int j=0; j<board[0].length; j++) {
-    			System.out.print(board[i][j] + " ");
-    		}
-    		System.out.println();
-    	}
-    	System.out.println();
+       for(int i=0; i<board.length; i++) {
+          for(int j=0; j<board[0].length; j++) {
+             System.out.print(board[i][j] + " ");
+          }
+          System.out.println();
+       }
+       System.out.println();
     }
 
     public static void main(String[] args) {
@@ -128,8 +146,9 @@ public class train_5_7_1KnightTracking_실습 {
         initializeBoard();
 
         // 나이트가 (0, 0)에서 시작
-        if (solveKnightTracking(0, 0)) {
+        if (solveKnightTracking(2, 2)) {
             showTracking();
+            System.out.println("해결완료");
         } else {
             System.out.println("해결할 수 없습니다.");
         }
