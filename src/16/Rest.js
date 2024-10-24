219ff971c1886c3fb5740aebd61525caa9010356
iambean-git <peb1021@naver.com>
2024-10-24 14:14:03 +0900

    게시판 restful

--- src/16/Rest.js
+++ src/16/Rest.js
@@ -6,8 +6,14 @@ export default function Rest() {
   const txt2Ref = useRef();
   const [tdata, setTdata] = useState([]);
   const [trs, setTrs] = useState([]);
-  const url = 'http://localhost:3005/posts';
 
+  const [isUpdate, setIsUpdate] = useState(false);  //수정 모드 확인하기
+  const [putId, setPutId] = useState();             //수정할 때 id를 저장하는 변수
+
+  
+
+  const url = 'http://localhost:3005/posts';
+  const idNum = 0;
   useEffect(()=>{
     getFetchData();   
   },[]);
@@ -16,7 +22,7 @@ export default function Rest() {
     const resp = await fetch(url);
     const data = await (resp.json());
 
-    console.log(data);
+    //console.log(data);
     setTdata(data);
   }
 
@@ -28,20 +34,130 @@ export default function Rest() {
             <td scope="col" className="px-6 py-3 w-1/6 font-normal text-center">
               <TailButton caption = "삭제"
                   color = "blue"
-                  handleClick = '' 
+                  handleClick = {()=>jsonDelete(i['id'])} 
                   size='' /> 
               </td>
             <td scope="col" className="px-6 py-3 w-1/6 font-normal text-center">
               <TailButton caption = "편집"
                   color = "orange"
-                  handleClick = '' 
+                  handleClick = {()=>jsonUpdate(i)} 
                   size='' /> 
-              
             </td>
           </tr>
     )
     setTrs(trsText);
+
+        
   },[tdata]);
+
+  const jsonPost = async() =>{
+
+    //입력공백확인
+    if(!txt1Ref.current.value){
+      alert("제목를 입력하세요.");
+      txt1Ref.current.focus();
+      return;
+    }
+    
+    if(txt2Ref.current.value === ''){
+      alert("작성자를 입력하세요.");
+      txt2Ref.current.focus();
+      return;
+    }
+
+    const postData = {
+      title : txt1Ref.current.value,
+      author : txt2Ref.current.value
+    }
+
+    const resp = await fetch(url, {
+      method : 'POST',
+      headers : {'Content-Type' : 'application/json'},
+      body : JSON.stringify(postData)
+    });
+
+    const data = await resp.json();
+    console.log(data);
+
+    setTdata([data, ...tdata]);
+    txt1Ref.current.value = '';
+    txt1Ref.current.focus();
+    txt2Ref.current.value = '';
+    
+  }
+
+  //게시글 삭제
+  const jsonDelete = async(id) =>{
+
+    const resp = await fetch(`${url}/${id}`,{
+      method : 'DELETE'
+    });
+    
+    //console.log("삭제된 데이터는 : ", await resp.json());
+
+    setTdata(tdata.filter(i=>i.id !== id));
+  }
+
+  //게시글 수정
+  const jsonUpdate = async(item) => {
+    setIsUpdate(true);
+
+    //console.log('수정하기 : ', item);
+    txt1Ref.current.value = item.title;
+    txt2Ref.current.value = item.author;
+    setPutId(item.id);
+  }
+
+  //게시글 수정
+  const jsonPut = async() => {
+    //console.log("수정하기 : handlePut >> ", putId);
+
+    //입력공백확인
+    if(!txt1Ref.current.value){
+      alert("제목를 입력하세요.");
+      txt1Ref.current.focus();
+      return;
+    }
+    
+    if(txt2Ref.current.value === ''){
+      alert("작성자를 입력하세요.");
+      txt2Ref.current.focus();
+      return;
+    }
+
+    const putData = {
+      title : txt1Ref.current.value,
+      author : txt2Ref.current.value
+    }
+
+    const resp = await fetch(`${url}/${putId}`, {
+      method : 'PUT',
+      headers : {'Content-Type' : 'application/json'},
+      body : JSON.stringify(putData)
+    });
+
+    const data = await resp.json();
+    console.log(data);
+
+    const tm = tdata.map(item=>item.id === putId ? data : item);
+
+
+    setTdata(tm);
+    txt1Ref.current.value = '';
+    txt1Ref.current.focus();
+    txt2Ref.current.value = '';
+
+    setIsUpdate(false);
+  }
+
+  //입력과 수정 구분하기
+  const handleOkBtn = () => {
+    if(!isUpdate)
+      jsonPost();
+    else
+      jsonPut();      
+  }
+
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <div className="w-11/12 grid grid-cols-1 md:grid-cols-7 
@@ -52,18 +168,18 @@ export default function Rest() {
           <input id="txt1"
             type="text" 
             className="form-input  w-full"
-            inRef={txt1Ref} />
+            ref={txt1Ref} />
         </div>
         <label htmlFor="txt2" className="my-2">작성자</label>
         <div className="flex">
           <input id="txt2"
             type="text"
             className="form-input w-full"
-            inRef={txt2Ref} />
+            ref={txt2Ref} />
         </div>
-        <TailButton caption = "입력"
+        <TailButton caption = {isUpdate? '수정' : '입력'}
                   color = "violet"
-                  handleClick = '' 
+                  handleClick = {handleOkBtn} 
                   size='' />  
       </div>
       <table
