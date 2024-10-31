7751b2c5f74d775a8c6c89366794bf2fa7d487dd
iambean-git <peb1021@naver.com>
2024-10-31 11:49:16 +0900

    객체 merge sort

--- src/dataStructure/chap06/train_실습과제6_2_객체merge정렬.java
+++ src/dataStructure/chap06/train_실습과제6_2_객체merge정렬.java
@@ -1,23 +1,63 @@
-package Chap6_Sorting;
+package dataStructure.chap06;
 
 /*
  * 6장 구현 실습과제2
  */
-/*
+
 class PhyscData implements Comparable<PhyscData>{
     String name;              // 이름
     int    height;            // 키
     double vision;            // 시력
-
-
-
+	
+    
+    public PhyscData(String name, int height, double vision) {
+		super();
+		this.name = name;
+		this.height = height;
+		this.vision = vision;
+	}
+    
+	@Override
+	public int compareTo(PhyscData o) {
+		if (name.compareTo(o.name) > 0)			return 1;
+		else if (name.compareTo(o.name) < 0)	return -1;
+		else {
+			if(height>o.height)			return 1;
+			else if(height<o.height)	return -1;
+			else {
+				if(vision>o.vision)			return 1;
+				else if(vision<o.vision)	return -1;
+				else	return 0;		//세가지 모두 같은 경우
+			}
+		}
+	}
 }
-*/
+
 public class train_실습과제6_2_객체merge정렬 {
 	// --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
 	static void merge(PhyscData[] a, int lefta, int righta, int leftb, int rightb ) {
-
-
+		PhyscData[] tmp = new PhyscData[rightb-lefta+1];
+		
+		int ix = 0; //tmp용 인덱스
+		int p = lefta, q=leftb;
+		while(p<=righta && q<=rightb) {
+			if(a[p].compareTo(a[q])<0)	tmp[ix++] = a[p++];
+			else if(a[p].compareTo(a[q])>0)	tmp[ix++] = a[q++];
+			else {
+				tmp[ix++] = a[p++];
+				tmp[ix++] = a[q++];
+			}
+		}
+		
+		while(p<=righta)	tmp[ix++] = a[p++];
+		while(q<=rightb)	tmp[ix++] = a[q++];		
+		
+		
+		//출력해보기
+		//System.out.println();
+		for(int j=0; j<ix; j++) {
+			a[lefta+j] = tmp[j];
+		}
 	}
 
 	// --- 퀵 정렬(비재귀 버전)---//
@@ -34,11 +74,13 @@ public class train_실습과제6_2_객체merge정렬 {
 		PhyscData[] x = {
 		         new PhyscData("강민하", 162, 0.3),
 		         new PhyscData("김찬우", 173, 0.7),
+		         new PhyscData("박준서", 180, 2.0),
 		         new PhyscData("박준서", 171, 2.0),
+		         new PhyscData("황지안", 169, 0.8),
 		         new PhyscData("유서범", 171, 1.5),
 		         new PhyscData("이수연", 168, 0.4),
 		         new PhyscData("장경오", 171, 1.2),
-		         new PhyscData("황지안", 169, 0.8),
+		        
 		     };
 		int nx = x.length;
 
